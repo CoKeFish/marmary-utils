e7cf04a516014d149e7293d83b17e1f611d9bf84
Rodión Romanovich Tabares Correa <37970518+CoKeFish@users.noreply.github.com>
2025-11-06 21:58:09 -0500

    Add event handling interfaces and execution methods for UI interactions

---
+++ Runtime/Events/EventInterfaces.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Marmary.Libraries.UI.Events
+{
+    /// <summary>
+    ///     IPressedHandler is an interface that is used by the UI system to send events when a UI element is pressed.
+    /// </summary>
+    public interface IPressedHandler : IEventSystemHandler
+    {
+        #region Event Functions
+
+        /// <summary>
+        /// Called when a UI element is pressed.
+        /// </summary>
+        /// <param name="eventData">The event data associated with the pointer press event.</param>
+        void OnPressed(PointerEventData eventData);
+
+        #endregion
+    }
+
+    /// <summary>
+    /// IUnPressedHandler is an interface that is used by the UI system to send events when a UI element is unpressed.
+    /// </summary>
+    public interface IUnPressedHandler : IEventSystemHandler
+    {
+        #region Event Functions
+
+        /// <summary>
+        /// Invoked when a UI element is unpressed, providing event data containing details of the pointer interaction.
+        /// </summary>
+        /// <param name="eventData">
+        /// The event data associated with the unpress action, including details such as pointer position, clicked object, and interaction states.
+        /// </param>
+        void OnUnPressed(PointerEventData eventData);
+
+        #endregion
+    }
+}
---
+++ Runtime/Events/MExecuteEvents.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine.EventSystems;
+using UExecuteEvents = UnityEngine.EventSystems.ExecuteEvents; // Alias
+
+namespace Marmary.Libraries.UI.Events
+{
+    /// <summary>
+    ///     This class is used to call the events of the interfaces.
+    /// </summary>
+    public static class MExecuteEvents
+    {
+        #region Properties
+
+        /// <summary>
+        ///     This is the event function for the IPressedHandler interface.
+        /// </summary>
+        public static UExecuteEvents.EventFunction<IPressedHandler> PressedHandler { get; } = Execute;
+
+        /// <summary>
+        ///     This is the event function for the IUnPressedHandler interface.
+        /// </summary>
+        public static UExecuteEvents.EventFunction<IUnPressedHandler> UnpressedHandler { get; } = Execute;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     This method is used to call the OnPressed method of the IPressedHandler interface.
+        /// </summary>
+        /// <param name="handler"> is the handler of the interface. </param>
+        /// <param name="eventData"> is the event data of the interface. </param>
+        private static void Execute(IPressedHandler handler, BaseEventData eventData)
+        {
+            handler.OnPressed(UExecuteEvents.ValidateEventData<PointerEventData>(eventData));
+        }
+
+        /// <summary>
+        ///     This method is used to call the OnUnPressed method of the IUnPressedHandler interface.
+        /// </summary>
+        /// <param name="handler"> is the handler of the interface. </param>
+        /// <param name="eventData"> is the event data of the interface. </param>
+        private static void Execute(IUnPressedHandler handler, BaseEventData eventData)
+        {
+            handler.OnUnPressed(UExecuteEvents.ValidateEventData<PointerEventData>(eventData));
+        }
+
+        #endregion
+    }
+}
