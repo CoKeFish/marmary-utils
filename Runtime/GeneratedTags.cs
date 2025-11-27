394d904c23ee36407fdeed80041f3ee3d6508645
Rodión Romanovich Tabares Correa <37970518+CoKeFish@users.noreply.github.com>
2025-11-26 22:23:49 -0500

    Add generated tags and assembly definition for Marmary.Utils

---
+++ Runtime/GeneratedTags.cs
@@ -0,0 +1,71 @@
+// THIS IS A GENERATED FILE BY THE EXTENDED DEBUG LOGS PACKAGE
+
+namespace DTT.ExtendedDebugLogs
+{
+	public static class SaveTags
+	{
+		public static Tag Saved => TagContainerCollection.GetContainer("Demo Tags").GetTag("SaveTags", "Saved");
+		public static Tag Loaded => TagContainerCollection.GetContainer("Demo Tags").GetTag("SaveTags", "Loaded");
+		public static Tag CorruptSave => TagContainerCollection.GetContainer("Demo Tags").GetTag("SaveTags", "CorruptSave");
+	}
+	public static class ShopTags
+	{
+		public static Tag CashShop => TagContainerCollection.GetContainer("Demo Tags").GetTag("ShopTags", "CashShop");
+		public static Tag FreeItems => TagContainerCollection.GetContainer("Demo Tags").GetTag("ShopTags", "FreeItems");
+		public static Tag PlayerItems => TagContainerCollection.GetContainer("Demo Tags").GetTag("ShopTags", "PlayerItems");
+	}
+	public static class UITags
+	{
+		public static Tag Settings => TagContainerCollection.GetContainer("Demo Tags").GetTag("UITags", "Settings");
+		public static Tag Menu => TagContainerCollection.GetContainer("Demo Tags").GetTag("UITags", "Menu");
+		public static Tag Paywall => TagContainerCollection.GetContainer("Demo Tags").GetTag("UITags", "Paywall");
+		public static Tag Saving => TagContainerCollection.GetContainer("Demo Tags").GetTag("UITags", "Saving");
+		public static Tag Loading => TagContainerCollection.GetContainer("Demo Tags").GetTag("UITags", "Loading");
+	}
+	public static class AnimationsTags
+	{
+		public static Tag NPC => TagContainerCollection.GetContainer("Demo Tags").GetTag("AnimationsTags", "NPC");
+		public static Tag Player => TagContainerCollection.GetContainer("Demo Tags").GetTag("AnimationsTags", "Player");
+		public static Tag Enemy => TagContainerCollection.GetContainer("Demo Tags").GetTag("AnimationsTags", "Enemy");
+	}
+	public static class SoundEffectsTags
+	{
+		public static Tag Environment => TagContainerCollection.GetContainer("Demo Tags").GetTag("SoundEffectsTags", "Environment");
+	}
+	public static class SettingTag
+	{
+		public static Tag Screen => TagContainerCollection.GetContainer("Marmary Tags").GetTag("SettingTag", "Screen");
+		public static Tag Render => TagContainerCollection.GetContainer("Marmary Tags").GetTag("SettingTag", "Render");
+	}
+	public static class FlowControlTag
+	{
+		public static Tag Event => TagContainerCollection.GetContainer("Marmary Tags").GetTag("FlowControlTag", "Event");
+		public static Tag Constructor => TagContainerCollection.GetContainer("Marmary Tags").GetTag("FlowControlTag", "Constructor");
+		public static Tag Setup => TagContainerCollection.GetContainer("Marmary Tags").GetTag("FlowControlTag", "Setup");
+		public static Tag EntryPoint => TagContainerCollection.GetContainer("Marmary Tags").GetTag("FlowControlTag", "EntryPoint");
+	}
+	public static class SaveTag
+	{
+		public static Tag Load => TagContainerCollection.GetContainer("Marmary Tags").GetTag("SaveTag", "Load");
+	}
+	public static class UITag
+	{
+		public static Tag Menu => TagContainerCollection.GetContainer("Marmary Tags").GetTag("UITag", "Menu");
+	}
+	public static class GameTag
+	{
+		public static Tag GridDebug => TagContainerCollection.GetContainer("Marmary Tags").GetTag("GameTag", "GridDebug");
+	}
+	public static class DataBaseTag
+	{
+		public static Tag StatsDeep => TagContainerCollection.GetContainer("Marmary Tags").GetTag("DataBaseTag", "StatsDeep");
+		public static Tag Stats => TagContainerCollection.GetContainer("Marmary Tags").GetTag("DataBaseTag", "Stats");
+	}
+	public static class GameplayTag
+	{
+		public static Tag GridDebug => TagContainerCollection.GetContainer("Marmary Tags").GetTag("GameplayTag", "GridDebug");
+		public static Tag Turn => TagContainerCollection.GetContainer("Marmary Tags").GetTag("GameplayTag", "Turn");
+		public static Tag Battle => TagContainerCollection.GetContainer("Marmary Tags").GetTag("GameplayTag", "Battle");
+		public static Tag Round => TagContainerCollection.GetContainer("Marmary Tags").GetTag("GameplayTag", "Round");
+	}
+}
